3684d5e6b829deed0336ab5f4d3d8a5cff165ff0
Gustavo Preilipper <gustavopreilipper.ifc.riodosul@gmail.com>
2023-08-22 15:24:13 -0300

    cad e login

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inika&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="UTF-8">
+    <title>Study n'Play</title>
+</head>
+<body id="body">
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand col-9" href="#">
+            <img src="assets/img/loguin.png" alt="Logo" height="64" class="d-inline-block align-text-top">
+            Study n' Play
+        </a>
+        <div>
+            <li>
+                <a class="navbar-brand "href="pages/login/login.html">Login</a>
+            </li>  
+        </div>
+        <div>
+            <li>
+                <a class="navbar-brand"href="pages/cadastro/cadastro.html">Cadastro</a>
+            </li>
+        </div>  
+    </div>
+</nav>
+<br> <br> <br>
+<div class="container">
+    <div class="row">
+        <div class="col-">
+        <h1>
+            Bem vindo ao <font class="urbanist" color="#4010A1">Study</font> n'<font color="#FF0000">P</font><font color="#FF9900">l</font><font color="00FF66">a</font><font color="2400FF">y</font>! 
+        </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+        <h1>
+            Por onde vamos começar hoje?
+        </h1>
+        </div> 
+    </div>
+    <div class="efeito-vidro col-10">
+        <div class="row">
+            <div class="col-12">
+                Não possui uma conta ainda?
+            </div>         
+        </div>
+        <div class="row">
+            <div class="col-4">
+                Considere se cadastrar!
+            </div>
+            <div class="col-7">
+                <form action="cadastro.php" method="post">
+                    <input type="text" name="email" id="email" placeholder="email">
+                    <button type="button" class="btn btn-dark">Entrar</button>
+                </form>
+                </div>
+               </div>
+               <div class="row">
+                   <div class="12">
+                       E rapidinho você terá acesso à
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="12">
+                       todos os recursos.
+                   </div>
+               </div>
+           </div>
+       </div>  
+<style>
+input[type=text] {
+height: 50px;
+width: 400px;
+position: relative;
+}
+</style>
+</body>
