e7835efc54b90267febb2e0ca838260b61c8e5f7
Gustavo Preilipper <gustavopreilipper.ifc.riodosul@gmail.com>
2023-12-11 15:47:33 -0300

    login

--- pages/login/login1.php
+++ pages/login/login1.php
@@ -1,27 +1,48 @@
 <?php
-    if (isset($_POST['nomUser'])) {
-        // Atribui o valor da variável 'nome_da_variavel' a uma nova variável
-        $minha_variavel = $_POST['nomUser'];
-    } else {
-        $minha_variavel = "";
-    }
+if (isset($_POST['nomUser'])) {
+    $minha_variavel = $_POST['nomUser'];
+} else {
+    $minha_variavel = "";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #343a40;
+            color: #fff;
+            
+        }
+
+        .card {
+            border: none;
+        }
+
+        .card-header {
+            background-color: #6f42c1;
+            color: #fff;
+        }
+
+        .glass-effect {
+            background-color: silver;
+            padding: 20px;
+           
+        }
+    </style>
 </head>
-<body class="text-white">
+<body>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header bg-purple">
-                        <h2 class="text-center">Login</h2>
+                    <div class="card-header text-center">
+                        <h2>Login</h2>
                     </div>
                     <div class="card-body glass-effect">
                         <form action="login.php" method="post">
@@ -38,8 +59,9 @@
                     </div>
                 </div>
             </div>
-        </div>j
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
